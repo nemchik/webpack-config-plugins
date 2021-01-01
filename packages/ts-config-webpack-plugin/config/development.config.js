332997ae9c46962851b65c4e8a6deab685546e73
Eric Nemchik <eric@nemchik.com>
2021-01-01 08:23:22 -0600

    Fix #18

--- packages/ts-config-webpack-plugin/config/development.config.js
+++ packages/ts-config-webpack-plugin/config/development.config.js
@@ -19,8 +19,8 @@ exports = module.exports = (options) => ({
 	module: {
 		rules: [
 			{
-				// .ts, .tsx, .d.ts
-				test: /\.(tsx?|d.ts)$/,
+				// .ts, .tsx
+				test: /\.(tsx?)$/,
 				use: [
 					{
 						// enable file based cache
